9dbb3bf64273adbd9ea6bf4ca78e3616932e35b2
ValentynTv <valentintw@gmail.com>
2021-04-21 13:59:09 +0300

    fix param passing

--- src/main/java/com/example/semanticapp/MainController.java
+++ src/main/java/com/example/semanticapp/MainController.java
@@ -22,15 +22,10 @@ public class MainController extends HttpServlet {
             throws ServletException, IOException {
         String email = request.getParameter("email");
         String password = request.getParameter("password");
-        boolean subscribed = Boolean.parseBoolean(request.getParameter("subscribed"));
 
-        String congratulationMessage;
-        if (subscribed) {
-            congratulationMessage = "Thanks for your subscription. We will send all the information to " + email;
-        } else {
-            congratulationMessage = "You are not pressed the subscription button so we will share your email: " + email +
-                    " and password: " + password + " all over the internet. Haha!";
-        }
+        String congratulationMessage = "We will share your email: " + email +
+                " and password: " + password + " all over the internet. Haha!";
+
         request.setAttribute("congratulationMessage", congratulationMessage);
         request.getRequestDispatcher("result.jsp").forward(request,response);
     }
